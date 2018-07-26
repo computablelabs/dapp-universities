ab0aac11459f9af82ffa327e93bbb08e1d47b7ed
Geoff Kinder <geoff.kinder@gmail.com>
2018-07-26 11:26:13 -0700

    Added placeholder pages.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,36 @@
-const Page = () => (
-  <h1>Next.js App</h1>
-);
+import React from 'react';
+import { connect } from 'react-redux';
 
-export default Page;
+import { address as getAddress } from 'reputable/dist/redux/selectors/token';
+
+import { Header } from '../components';
+
+class Page extends React.Component {
+  static async getInitialProps({ ctx }) {
+    console.log('index fetch initial props: ', ctx);
+
+    return {};
+  }
+
+  componentDidMount() {
+    console.log('index mounted: ', this.props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Next.js App</h1>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  address: getAddress(state),
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
 
