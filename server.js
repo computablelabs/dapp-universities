19c01f45ea0a30bb1c56aa14df41501790f57e9e
Geoff Kinder <geoff.kinder@gmail.com>
2018-08-21 13:41:23 -0700

    Updated server to support dynamic urls + POST to a search endpoint.

--- server.js
+++ server.js
@@ -7,10 +7,28 @@ const dev = env !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const { submitQuery } = require('./utils');
+
 app.prepare()
   .then(() => {
     const server = express();
 
+    // Handle search requests
+    server.post('/search/:query', async (req, res) => {
+      const queryResponse = await submitQuery(req.params.query);
+      const results = await queryResponse.toPromise();
+
+      res.send(JSON.stringify({ response: results }));
+    });
+
+    server.get('/listings/:query', (req, res) => {
+      const actualPage = '/listings';
+      const queryParams = { query: req.params.query };
+
+      app.render(req, res, actualPage, queryParams);
+    });
+
+    // Handle all other requests
     server.get('*', (req, res) => handle(req, res));
 
     server.listen(3000, (err) => {
