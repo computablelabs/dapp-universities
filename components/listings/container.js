48c4f37b65043990b059e463cd2542aa6ef0749a
Geoff Kinder <geoff.kinder@gmail.com>
2018-08-31 11:56:55 -0700

    Updated <Listings> to support sort and limit filtering.

--- components/listings/container.js
+++ components/listings/container.js
@@ -4,9 +4,35 @@ import { bindActionCreators } from 'redux';
 
 import { getApplicants } from 'reputable/dist/redux/selectors';
 
-const mapStateToProps = (state) => ({
-  applicants: getApplicants(state),
-});
+const mapStateToProps = (state, props) => {
+  let applicants = getApplicants(state);
+
+  if (props.sortBy) {
+    const sort = (a, b) => {
+      const attr = props.sortBy;
+      const attr1 = a.data.value[attr];
+      const attr2 = b.data.value[attr];
+
+      if (attr1 < attr2) {
+        return -1;
+      }
+
+      if (attr1 > attr2) {
+        return 1;
+      }
+
+      return 0;
+    };
+
+    applicants = applicants.sort(sort);
+  }
+
+  if (props.limit) {
+    applicants = applicants.slice(0, props.limit);
+  }
+
+  return { applicants };
+};
 
 const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
 
