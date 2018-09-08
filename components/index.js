addec1aeff2bdc603a4843958081b3de4decf0ea
Geoff Kinder <geoff.kinder@gmail.com>
2018-09-07 17:39:40 -0700

    Removed old <Listings> component.

--- components/index.js
+++ components/index.js
@@ -1,6 +1,5 @@
 export * from './forms';
 export { default as Header } from './header';
-export { default as Listings } from './listings';
 export { default as AppliedListings } from './appliedListings';
 export { default as WhitelistedListings } from './whitelistedListings';
 
