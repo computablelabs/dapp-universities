2f64f0c851ee701d64f9fe50b6c4e01221959ddf
Caroline Sofiatti <c.sofiatti@gmail.com>
2018-09-18 15:34:45 -0700

    Doubled the APPLY_STAGE_LEN for demo

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const APPLY_STAGE_LENGTH = 5;   // in seconds
+const APPLY_STAGE_LENGTH = 10;   // in seconds
 const COMMIT_STAGE_LENGTH = 1;  // in seconds
 const REVEAL_STAGE_LENGTH = 1;  // in seconds
 
