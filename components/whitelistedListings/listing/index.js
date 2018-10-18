205d1a222a15c8a0d0269773b061d559eded6f86
Geoff Kinder <geoff.kinder@gmail.com>
2018-10-18 14:09:15 -0700

    Added the ability for a user to remove a whitelisted listing.

--- components/whitelistedListings/listing/index.js
+++ components/whitelistedListings/listing/index.js
@@ -2,6 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Listing extends React.Component {
+  constructor() {
+    super();
+
+    this.handleRemoveListing = this.handleRemoveListing.bind(this);
+  }
+
+  handleRemoveListing() {
+    const {
+      listingHash,
+      owner,
+      name,
+      onRemoveListing,
+    } = this.props;
+
+    // Note:
+    //   This should be the current user's address, not the listing's owner.
+    //   Since this is a single user system, we are skipping this validation.
+    onRemoveListing({
+      listingHash,
+      userAddress: owner,
+      name,
+    });
+  }
+
   render() {
     const { name, rank } = this.props;
 
@@ -21,17 +45,33 @@ class Listing extends React.Component {
 
         <td>{name}</td>
         <td>{rank}</td>
+        <td>
+          {
+            true && (
+              <button onClick={this.handleRemoveListing}>
+                Remove
+              </button>
+            )
+          }
+        </td>
       </React.Fragment>
     );
   }
 }
 
 Listing.propTypes = {
+  listingHash: PropTypes.string.isRequired,
+  owner: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   rank: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
   ]).isRequired,
+  onRemoveListing: PropTypes.func,
+};
+
+Listing.defaultProps = {
+  onRemoveListing: () => {},
 };
 
 export default Listing;
